34d5d09622500a7cee81090f7c113b8146273cbd
Kiran <kiran.sadara6730@gmail.com>
2026-05-25 18:29:33 +0530

    Kafka for email of forgot password is done

--- Auth_backend/app/service/forgotPassword_Service.js
+++ Auth_backend/app/service/forgotPassword_Service.js
@@ -1,5 +1,6 @@
 import prisma from "../../global/db_connection.js";
 import crypto from "crypto";
+import { producer } from "../../../kafka/index.js";
 
 export const forgot_password_service = async (email) => {
 
@@ -36,8 +37,59 @@ export const forgot_password_service = async (email) => {
         });
 
         // send email here
+
+        producer.send({
+            topic: "email-service-topic",
+            messages: [
+               
+                {
+
+                    key : "forgot-password",
+            
+            value : JSON.stringify({ 
+                
+            from: "eventnest.official.main@gmail.com",
+            to: email,
+            subject: "Password Change Code ",
+            text: "Please find the code to reset the password.",
+            html: `
+               
+           <div style="font-family: Arial, sans-serif; padding: 20px; border: 2px solid #4CAF50; border-radius: 10px; max-width: 600px; margin: auto; background-color: #f9f9f9;">
+      
+      <h1 style="color: #4CAF50; text-align: center;margin-bottom:20px;">
+        Enter this code to reset your password
+      </h1>
+
+      <div style="text-align:center;">
+        <div style="
+          display: inline-block;
+          padding: 12px 25px;
+          background-color: #ffffff;
+          border: 2px dashed #4CAF50;
+          border-radius: 8px;
+          font-size: 22px;
+          letter-spacing: 4px;
+          font-weight: bold;
+          color: #333;">
+          ${otp}
+        </div>
+      </div>
+
+      <p style="color:#555; text-align:center; margin-top:25px; font-size: 14px;">
+        If you did not request this, you can safely ignore this email.
+      </p>
+
+    </div>
+            ` 
+
+
+        })
+
+    }
+            
+        ] });
        
-        return reset_link;
+        return true;
 
     } catch (er) {
         throw er;
--- Email_service_backend/app/services/Auth_Email_Service.js
+++ Email_service_backend/app/services/Auth_Email_Service.js
@@ -3,11 +3,9 @@ import transporter from "../transporter.js";
 
 export const Auth_Email_Servive = async (data) => {
     try {
-        console.log("Data received in Email Service:", data);
-
+       
         const info = await transporter.sendMail(data);
 
-        console.log("EMAIL SENT SUCCESS:", info.messageId);
 
         return true;
     } catch (er) {
